5b3ce820ed8680ee252cbde71fa8da348a903695
hariom-techculture <techcultureitsolutions@gmail.com>
2025-09-18 17:15:01 +0530

    change cors policy

--- index.js
+++ index.js
@@ -28,13 +28,14 @@ const PORT = process.env.PORT || 5000
 app.use(
   cors({
     origin: [
+      "https://appcenter.techculture.ai",
+      "https://techculture.ai",
       "http://appcenter.techculture.ai",
       "http://techculture.ai",
       "http://localhost:3000",
       "http://localhost:3001",
-      "http://192.168.1.228:3000",
       "http://165.22.217.110:3001",
-      "http://165.22.217.110:3000"
+      "http://165.22.217.110:3000",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
